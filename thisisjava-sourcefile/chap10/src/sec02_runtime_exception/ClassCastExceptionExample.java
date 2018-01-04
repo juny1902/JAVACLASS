86c10d369a2fa64559f42ff54f20b1ead9e83d7b
unknown <juny1902@naver.com>
2018-01-04 10:33:30 +0900

    180104

--- thisisjava-sourcefile/chap10/src/sec02_runtime_exception/ClassCastExceptionExample.java
+++ thisisjava-sourcefile/chap10/src/sec02_runtime_exception/ClassCastExceptionExample.java
@@ -10,13 +10,23 @@ public class ClassCastExceptionExample {
 	}
 	
 	public static void changeDog(Animal animal) {
-		//if(animal instanceof Dog) {
+		if(animal instanceof Dog) {
 			Dog dog = (Dog) animal; 				//ClassCastException 惯积 啊瓷
-		//} 
+		}else{
+			System.out.println("抗寇 惯积.");
+		}
 	}
 }
 
 class Animal {}
-class Dog extends Animal {}
-class Cat extends Animal {}
+class Dog extends Animal {
+	public Dog(){
+		System.out.println("港港");
+	}
+}
+class Cat extends Animal {
+	public Cat(){
+		System.out.println("具克");
+	}
+}
 
--- thisisjava-sourcefile/chap10/src/sec03_try_catch_finally/TryCatchFinallyExample.java
+++ thisisjava-sourcefile/chap10/src/sec03_try_catch_finally/TryCatchFinallyExample.java
@@ -4,6 +4,7 @@ public class TryCatchFinallyExample {
 	public static void main(String[] args) {
 		try {
 			Class clazz = Class.forName("java.lang.String2");
+			System.out.println(clazz.getName());
 		} catch(ClassNotFoundException e) {
 			System.out.println("클래스가 존재하지 않습니다.");
 		}
