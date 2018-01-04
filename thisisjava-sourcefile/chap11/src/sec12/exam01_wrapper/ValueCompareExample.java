758641bc50fa7dbb3c73a443d898832b57948fd0
unknown <juny1902@naver.com>
2018-01-04 12:53:31 +0900

    backup

--- thisisjava-sourcefile/chap11/src/sec12/exam01_wrapper/ValueCompareExample.java
+++ thisisjava-sourcefile/chap11/src/sec12/exam01_wrapper/ValueCompareExample.java
@@ -5,7 +5,7 @@ public class ValueCompareExample {
 		System.out.println("[-128~127 초과값일 경우]");
 		Integer obj1 = 300;
 		Integer obj2 = 300;
-		System.out.println("==결과: " + (obj1 == obj2));
+		System.out.println("==결과: " + (obj1 == obj2)); 
 		System.out.println("언박싱후 ==결과: " + (obj1.intValue() == obj2.intValue()));
 		System.out.println("equals() 결과: " + obj1.equals(obj2));
 		System.out.println();
@@ -16,5 +16,5 @@ public class ValueCompareExample {
 		System.out.println("==결과: " + (obj3 == obj4));
 		System.out.println("언박싱후 ==결과: " + (obj3.intValue() == obj4.intValue()));
 		System.out.println("equals() 결과: " + obj3.equals(obj4));
-	}
+	}	
 }
